379694892c56b406868dc44d358fbc3e92520602
NicholasFarquhar1 <166021449+NicholasFarquhar1@users.noreply.github.com>
2025-05-15 09:25:39 +0200

    Final fixes for the emotions and interactions

--- BasicQuestoinHandler.cs
+++ BasicQuestoinHandler.cs
@@ -3,7 +3,6 @@ using System.Text.RegularExpressions;
 
 namespace ST10439055_PROG_POE
 {
-    //handles basic chatbot questions
     public class BasicQuestionsHandler : UserInteraction
     {
         
